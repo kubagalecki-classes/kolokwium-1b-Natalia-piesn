ee7249e6e2d516d8f9a7fc95316f5103c3a71ff2
Nataliapiesn <01123786@pw.edu.pl>
2021-01-15 08:34:54 +0000

    2-2

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -11,7 +11,7 @@ class PopGwiazda : public Artysta
 public:
   PopGwiazda(const string& s, const unsigned& n) : pseudonim(s), sluchacze(n){}
   void graj(std::ostream& stream) const {
-    stream << "Popgwiazda: " << pseudonim;
+    stream << "Popgwiazda: " << pseudonim();
   }
 private:
   string pseudonim;
@@ -22,7 +22,7 @@ class RapGwiazda : public Artysta
 public:
   RapGwiazda(const string& s, const unsigned& n) : pseudonim(s), sluchacze(n){}
   void graj(std::ostream& stream) const {
-    stream << "Rapgwiazda: " << getPseudonim;
+    stream << "Rapgwiazda: " << getPseudonim();
   }
 private:
   string pseudonim;
