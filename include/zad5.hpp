e607303cd24b583137f9584ba0319c1c744516c4
Nataliapiesn <01123786@pw.edu.pl>
2021-01-15 08:29:20 +0000

    5-2

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -6,7 +6,7 @@
 
 // tutaj algorytm liczPopularnych(T begin, T end)
 using namespace std;
-template <Artysta T>
+template <class T>
 int liczPopularnych(T it_b, T it_e)
 {
   return count_if(it_b, it_e, [](T a){return a.getSluchacze()>1000;});
